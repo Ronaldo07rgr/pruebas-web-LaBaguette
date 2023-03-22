6a3a584fef8ac5c9494f358331a1667e6e04e0e6
GUANKI <jm065726@gmail.com>
2023-03-22 11:00:10 -0500

    pagina web

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const cartIcon = document.getElementById('cart-icon');
+const cartMenu = document.getElementById('cart-menu');
+
+cartIcon.addEventListener('click', function() {
+  cartMenu.style.display = 'block';
+});
